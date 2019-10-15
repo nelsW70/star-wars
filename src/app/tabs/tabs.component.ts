3d8693ac2d4e68b140d0234cd67e59b4fb5ac41a
Nelson Foltz <NFoltz@ppg.com>
2019-10-15 11:22:33 -0400

    basic logic for displaying initial list

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.component.css']
 })
 export class TabsComponent implements OnInit {
+  characters = [
+    { name: 'Luke Skywalker', side: '' },
+    { name: 'Darth Vader', side: '' }
+  ];
 
-  constructor() { }
+  chosenList = 'all';
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  onChoose(side: string) {
+    this.chosenList = side;
   }
 
+  getCharacters() {
+    if (this.chosenList === 'all') {
+      return this.characters.slice();
+    }
+    return this.characters.filter(char => {
+      return char.side === this.chosenList;
+    });
+  }
 }
