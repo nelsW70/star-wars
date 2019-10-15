6c602594148ad16adede211db9b25e66a2c7d5b0
Nelson Foltz <NFoltz@ppg.com>
2019-10-15 14:52:00 -0400

    refactor to service for managing state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,12 @@ import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
 import { ListComponent } from './list/list.component';
 import { ItemComponent } from './item/item.component';
+import { StarWarsService } from './star-wars.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TabsComponent,
-    ListComponent,
-    ItemComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, TabsComponent, ListComponent, ItemComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [StarWarsService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
