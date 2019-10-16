554038b39637ffa9c8662e5366549d621bb05f67
Nelson Foltz <NFoltz@ppg.com>
2019-10-16 15:35:13 -0400

    ondestroy for subscribe

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StarWarsService } from './../star-wars.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, OnDestroy {
   characters = [];
   activatedRoute: ActivatedRoute;
   swService: StarWarsService;
   loadedSide = 'all';
+  subscription: Subscription;
 
   constructor(activatedRoute: ActivatedRoute, swService: StarWarsService) {
     this.activatedRoute = activatedRoute;
@@ -24,8 +26,12 @@ export class ListComponent implements OnInit {
       this.loadedSide = params.side;
     });
 
-    this.swService.charactersChanged.subscribe(() => {
+    this.subscription = this.swService.charactersChanged.subscribe(() => {
       this.characters = this.swService.getCharacters(this.loadedSide);
     });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
